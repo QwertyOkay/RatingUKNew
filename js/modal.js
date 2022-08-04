cab21d510a9ce0c39d92a162dbeed855dad0fa01
Alexandr Kotenko <60352683+QwertyOkay@users.noreply.github.com>
2022-08-04 14:12:11 +0300

    update

--- js/modal.js
+++ js/modal.js
@@ -4,16 +4,19 @@
       openModalBtn2: document.querySelector('[data-modal-open2]'),
       openModalBtn3: document.querySelector('[data-modal-open3]'),
       openModalBtn4: document.querySelector('[data-modal-open4]'),
+      openModalBtn5: document.querySelector('[data-modal-open5]'),
       
       closeModalBtn: document.querySelector('[data-modal-close]'),
       closeModalBtn2: document.querySelector('[data-modal-close2]'),
        closeModalBtn3: document.querySelector('[data-modal-close3]'),
        closeModalBtn4: document.querySelector('[data-modal-close4]'),
+       closeModalBtn5: document.querySelector('[data-modal-close5]'),
 
       modal: document.querySelector('[data-modal]'),
       modal2: document.querySelector('[data-modal2]'),
       modal3: document.querySelector('[data-modal3]'),
       modal4: document.querySelector('[data-modal4]'),
+      modal5: document.querySelector('[data-modal5]'),
     };
   
   refs.openModalBtn.addEventListener('click', toggleModal);
@@ -27,6 +30,9 @@
 
   refs.openModalBtn4.addEventListener('click', toggleModal4);
   refs.closeModalBtn4.addEventListener('click', toggleModal4);
+
+  refs.openModalBtn5.addEventListener('click', toggleModal5);
+  refs.closeModalBtn5.addEventListener('click', toggleModal5);
   
     function toggleModal() {
       refs.modal.classList.toggle('is-hidden');
@@ -42,6 +48,10 @@
 
   function toggleModal4() {
     refs.modal4.classList.toggle('is-hidden4');
+}
+
+function toggleModal5() {
+  refs.modal5.classList.toggle('is-hidden5');
 }
   
   })();
